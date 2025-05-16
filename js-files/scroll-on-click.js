cc83c5a316fcfacd59f44098f9fb9588e4c4f746
cmillerl <cmi1779@wgu.edu>
2025-05-16 15:42:10 -0700

    Fix scroll-on-click function and add 300 ms delay to adjust for dyanmic loading of header and footer.

--- js-files/scroll-on-click.js
+++ js-files/scroll-on-click.js
@@ -1,29 +1,39 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const navLinks = document.querySelectorAll('.nav-list a');
-    const headerHeight = document.querySelector('header').offsetHeight;
+    setTimeout(function() {
+        const navLinks = document.querySelectorAll('.nav-list a');
+        const header = document.querySelector('header');
 
-    navLinks.forEach(link => {
-        link.addEventListener('click', function (e) {
-            e.preventDefault();
-            const targetId = this.getAttribute('href');
-            const targetElement = document.querySelector(targetId);
+        if (header && navLinks.length > 0) {
+            const headerHeight = header.offsetHeight;
 
-            if (targetId === "#home") {
-                e.preventDefault();
-                window.scrollTo({
-                    top: 0,
-                    behavior: 'smooth'
+            navLinks.forEach(link => {
+                link.addEventListener('click', function (e) {
+                    const targetId = this.getAttribute('href');
+                    
+                    if (targetId.startsWith('#')) {
+                        e.preventDefault();
+                        
+                        if (targetId === "#home") {
+                            window.scrollTo({
+                                top: 0,
+                                behavior: 'smooth'
+                            });
+                        } else {
+                            const targetElement = document.querySelector(targetId);
+                            
+                            if (targetElement) {
+                                const targetPosition = targetElement.getBoundingClientRect().top;
+                                const offsetPosition = targetPosition + window.pageYOffset - headerHeight - 10;
+                                
+                                window.scrollTo({
+                                    top: offsetPosition,
+                                    behavior: 'smooth'
+                                });
+                            }
+                        }
+                    }
                 });
-            }
-
-            if (targetElement) {
-                const targetPosition = targetElement.getBoundingClientRect().top;
-                const offsetPosition = targetPosition + window.pageYOffset - headerHeight - 10;
-                window.scrollTo({
-                    top: offsetPosition,
-                    behavior: 'smooth'
-                });
-            }
-        });
-    });
+            });
+        }
+    }, 300);
 });
