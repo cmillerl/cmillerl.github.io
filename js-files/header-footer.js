6a74d69e941c81990de250f36dbd350c9aeb264e
cmillerl <cmi1779@wgu.edu>
2025-06-09 15:50:59 -0700

    Formatting.

--- js-files/header-footer.js
+++ js-files/header-footer.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     fetch('header.html').then(response => response.text()).then(data => {
         document.getElementById('header-placeholder').innerHTML = data;
     })
-    
+
     fetch('footer.html').then(response => response.text()).then(data => {
         document.getElementById('footer-placeholder').innerHTML = data;
     })
