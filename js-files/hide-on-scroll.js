1ba408ab5472d34772859523f02a94c55d84acb8
cmillerl <cmi1779@wgu.edu>
2025-06-11 15:03:32 -0700

    Fix hide-on-scroll logic.

--- js-files/hide-on-scroll.js
+++ js-files/hide-on-scroll.js
@@ -1,36 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    let header = null;
     let currentRetries = 0;
     const maxRetries = 10;
     let currentPosition = window.pageYOffset;
-
+    let scrollTimeout;
 
     function hideHeader() {
+        const header = document.querySelector("header");
+
         if (!header && currentRetries < maxRetries) {
-            header = document.querySelector("header");
             currentRetries++;
             setTimeout(hideHeader, 100);
         } else if (header) {
             window.addEventListener("scroll", function () {
-                let newPosition = window.pageYOffset;
+                clearTimeout(scrollTimeout);
 
-                if (currentPosition < newPosition) {
-                    this.setTimeout(() => { 
-                        header.style.transition = "all 0.6s ease-in-out";
-                        header.style.top = "-100px";
-                    }, 300);
-                } else {
-                    this.setTimeout(() => { 
-                        header.style.transition = "all 0.6s ease-in-out";
-                        header.style.top = "0px";
-                    }, 300);
-                }
-                currentPosition = newPosition;
-            })
+                scrollTimeout = setTimeout(() => {
+                    let newPosition = window.pageYOffset;
+
+                    if (currentPosition < newPosition && newPosition > 100) {
+                        header.style.transition = "all 0.3s ease-in-out";
+                        header.style.transform = "translateY(-100%)";
+                    } else if (currentPosition > newPosition) {
+                        header.style.transition = "all 0.3s ease-in-out";
+                        header.style.transform = "translateY(0)";
+                    }
+
+                    currentPosition = newPosition;
+                }, 100);
+            });
         }
     }
 
-hideHeader();
+    hideHeader();
 
 });
